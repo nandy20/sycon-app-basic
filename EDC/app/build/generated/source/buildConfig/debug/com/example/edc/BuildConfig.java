958bddd47a9b8fe99e41b18d571603e71041a9b1
karthikviz2000 <47050339+karthikviz2000@users.noreply.github.com>
2019-01-26 11:22:06 +0530

    Add files via upload

---
+++ EDC/app/build/generated/source/buildConfig/debug/com/example/edc/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.edc;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.edc";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
